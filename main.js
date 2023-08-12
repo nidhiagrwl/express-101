f66c04b321d207674270e8fe218313a72c6dc8d2
Nidhiagr <nidhi.agrawal@jobtwine.com>
2023-08-13 00:46:56 +0530

    rendering page

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const path = require('path');
+ 
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 app.get('/about', (req, res) => {
-  res.send('About PAge')
+  res.send('About PAge') // sending text
 })
+app.get('/aboutpage',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html')); //sending  file
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
